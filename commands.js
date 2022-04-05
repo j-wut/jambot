312a5c97a7b64045655558a44e19e6a18287da6c
James Wu <james.wut@gmail.com>
2022-04-05 03:09:03 -0400

    inserting args into other args

--- commands.js
+++ commands.js
@@ -16,8 +16,6 @@ export function createConst(channel, tags, args, client) {
     }
 }
 
-const commandConstMatcher = /\{\{(\s+)\}\}/;
-
 export function createCommand(channel, tags, args, client) {
     if (isMod(tags)) {
         client.say(channel, `command ${args[0]} created`);
@@ -26,8 +24,12 @@ export function createCommand(channel, tags, args, client) {
     }
 }
 
+export function insertArg(channel, tags, args, client) {
+    console.log("insertParamter");
+    client.say(channel, insertArgs(args[0], args.slice(1)));
+}
 
-const argMatcher = /((?:(?:'.*')|(?:".*")|(?:`.*`)|(?:[^'"`,\s]+)))[,\s]*/g;
+const argMatcher = /((?:(?:'.*?')|(?:".*?")|(?:`.*?`)|(?:[^'"`,\s]+)))[,\s]*/g;
 
 export const argsParser = (argString) => {
     const matches = argString.trim().matchAll(argMatcher);
@@ -42,9 +44,32 @@ export const argsParser = (argString) => {
 }
 
 
+const insertArgMatcher = /\[\[([0-9]+)\]\]/g
+export const insertArgs = (inputString, argList, allowStrings = true) => {
+    const matches = inputString.matchAll(insertArgMatcher);
+    let currentMatcher = matches.next();
+    let currentResult = inputString;
+    console.log(currentMatcher);
+    while(currentMatcher.value && !currentMatcher.done) {
+        const argIndex = Number(currentMatcher.value[1]);
+        const argToInsert = argList[Number(currentMatcher.value[1])];
+        console.log ("argIndex",argIndex, "argToInsert", "argToInsert");
+        if(!allowStrings && (argToInsert.includes('"') || argToInsert.includes('`') || argToInsert.includes('\''))){
+            throw new Error("Quotes not allowed here");
+        }
+        currentResult = currentResult.replace(`[[${argIndex}]]`, isStringArg(argToInsert) ? argToInsert.substring(1,argToInsert.length-1) : argToInsert );
+        console.log(currentResult);
+        currentMatcher = matches.next()
+    }
+    return currentResult;
+}
 
+const isStringArg = (input) => {
+    return input && input[0] === input[input.length-1] && ['"', '`', '\''].includes(input[0]);
+}
 
 export default {
     hai,
     createConst,
+    insertArg
 }
